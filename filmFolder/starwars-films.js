b0d0fedc689c73efe6789c0f4eec264a679a542e
BatesCubed <batescubed@gmail.com>
2022-11-03 14:33:44 -0600

    keeping things consistent with the example code

--- filmFolder/starwars-films.js
+++ filmFolder/starwars-films.js
@@ -1,13 +1,23 @@
 import { films } from '../data/films.js'
 
-console.log('This is my first JavaScript console message!')
-
-let filmOne = films[0]
-
-let filmList = document.querySelector('#filmList')
+let filmList = document.querySelector('#filmList') 
 
 for (let i = 0; i < films.length; i++) {
-    let newParagraph = document.createElement('p')
-    newParagraph.textContent = films[i].title
-    filmList.appendChild(newParagraph)
+  let figure = document.createElement('figure')
+  let figImage = document.createElement('img')
+  let figCaption = document.createElement('figcaption')
+
+  let filmNum = getLastNumber(films[i].url)
+
+  figImage.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg`
+ 
+  figCaption.textContent = films[i].title
+  
+  figure.appendChild(figImage)
+  figure.appendChild(figCaption)
+  filmList.appendChild(figure)
+}
+
+function getLastNumber(url) {
+  return url.slice(url.length - 2, url.length - 1)
 }
