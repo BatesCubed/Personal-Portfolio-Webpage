b0adc393d3ad400c5f7deed1344099a69ef84003
BatesCubed <batescubed@gmail.com>
2022-12-12 09:52:26 -0700

    Created a better layout

--- finalProject/rings.js
+++ finalProject/rings.js
@@ -6,12 +6,18 @@ for (let i = 0; i < ringObj.length; i++) {
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
     let figCaption = document.createElement('figcaption')
+    let figP = document.createElement('p')
 
     figImg.src = ringObj[i].ringImg
 
     figCaption.textContent = ringObj[i].ringName
-
+    figP.textContent = '$' + ringObj[i].ringPrice
+    figure.appendChild(figP)
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
     ringShop.appendChild(figure)
 }
+
+figure.addEventListener('click', () => {
+    
+})
