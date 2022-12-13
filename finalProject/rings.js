a60ab60cf2e379fee90d869548bcf3ebda43c1da
BatesCubed <batescubed@gmail.com>
2022-12-12 20:56:17 -0700

    Populated Pop Up correctly for each item

--- finalProject/rings.js
+++ finalProject/rings.js
@@ -1,27 +1,55 @@
 import { ringObj } from '../data/items.js'
 
-let ringShop = document.querySelector('#ringSection')
+var ringShop = document.querySelector('#ringSection')
 
 const backFocus = document.querySelector('.backgroundFocus')
 const itemFocus = document.querySelector('.itemFocus')
+const exitButton = document.querySelector('.xButton')
+
+
+exitButton.addEventListener('click', () => {
+    backFocus.style.display = "none";
+    itemFocus.style.display = "none";
+})
+
+
 
 for (let i = 0; i < ringObj.length; i++) {
     const figure = document.createElement('figure')
     const figImg = document.createElement('img')
     const figCaption = document.createElement('figcaption')
     const figP = document.createElement('p')
+    const ringNum = ringObj[i].ringNum
+   
 
     figImg.src = ringObj[i].ringImg
 
     figCaption.textContent = ringObj[i].ringName
     figP.textContent = '$' + ringObj[i].ringPrice
+    
     figure.appendChild(figP)
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
     ringShop.appendChild(figure)
-    
+    figure.classList.toggle(ringNum)
+
     figure.addEventListener('click', () => {
         backFocus.style.display = "inline";
         itemFocus.style.display = "inline";
+   
+   
+        const ringNum = figure.className
+    const focusName = document.querySelector('.ringName')
+    const focusImg = document.querySelector('.ringImage')
+    const focusDesc = document.querySelector('.ringDesc')
+    const focusPrice = document.querySelector('.ringPrice')
+
+        focusName.textContent = ringObj[ringNum].ringName
+        focusImg.src = ringObj[ringNum].ringImg
+        focusDesc.textContent = ringObj[ringNum].ringDesc
+        focusPrice.textContent = '$' + ringObj[ringNum].ringPrice
     })
 }
+
+
+ 
