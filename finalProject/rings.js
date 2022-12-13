1b07f1930e52ecf5264062f00bb7988b3f9e835d
BatesCubed <batescubed@gmail.com>
2022-12-12 22:08:21 -0700

    Added a somewhat working cart array...

--- finalProject/rings.js
+++ finalProject/rings.js
@@ -5,7 +5,9 @@ var ringShop = document.querySelector('#ringSection')
 const backFocus = document.querySelector('.backgroundFocus')
 const itemFocus = document.querySelector('.itemFocus')
 const exitButton = document.querySelector('.xButton')
-
+const cart = document.querySelector('.addToCart')
+const cartInv = [];
+const cartAmt = document.querySelector('.amount')
 
 exitButton.addEventListener('click', () => {
     backFocus.style.display = "none";
@@ -20,7 +22,7 @@ for (let i = 0; i < ringObj.length; i++) {
     const figCaption = document.createElement('figcaption')
     const figP = document.createElement('p')
     const ringNum = ringObj[i].ringNum
-   
+    
 
     figImg.src = ringObj[i].ringImg
 
@@ -33,23 +35,53 @@ for (let i = 0; i < ringObj.length; i++) {
     ringShop.appendChild(figure)
     figure.classList.toggle(ringNum)
 
+    
+
     figure.addEventListener('click', () => {
         backFocus.style.display = "inline";
         itemFocus.style.display = "inline";
    
    
         const ringNum = figure.className
-    const focusName = document.querySelector('.ringName')
-    const focusImg = document.querySelector('.ringImage')
-    const focusDesc = document.querySelector('.ringDesc')
-    const focusPrice = document.querySelector('.ringPrice')
+        const focusName = document.querySelector('.ringName')
+        const focusImg = document.querySelector('.ringImage')
+        const focusDesc = document.querySelector('.ringDesc')
+        const focusPrice = document.querySelector('.ringPrice')
+        
+
 
         focusName.textContent = ringObj[ringNum].ringName
         focusImg.src = ringObj[ringNum].ringImg
         focusDesc.textContent = ringObj[ringNum].ringDesc
         focusPrice.textContent = '$' + ringObj[ringNum].ringPrice
+
+        cart.addEventListener('click', () => {
+            cartInv.push(ringObj[ringNum])
+            cartAmt.textContent = cartInv.length
+            return cartInv.length
+        })
     })
 }
 
 
+const cartView = document.querySelector('.cartSection')
+
+for (let i = 0; i < cartInv.length; i++) {
+    const cartFigure = document.createElement('figure')
+    const cartImg = document.createElement('img')
+    const cartName = document.createElement('h1')
+    const cartPrice = document.createElement('p')
+    const cartTotal = document.createElement('p')
+    
+
+    cartImg.src = cartInv[i].ringImg
+    cartName.textContent = cartInv[i].ringName
+    cartPrice.textContent = cartInv[i].ringPrice
+    
+    cartFigure.appendChild(cartImg)
+    cartFigure.appendChild(cartName)
+    cartFigure.appendChild(cartPrice)
+    cartView.appendChild(cartFigure)
+
+}
  
